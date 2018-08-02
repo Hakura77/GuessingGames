c9c7ed77136d725dfbc4ea967d81225be0834b4c
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-08-02 17:31:56 +1200

    StandardJS update and Attributation

--- src/SimpleView.js
+++ src/SimpleView.js
@@ -1,39 +1,40 @@
-class SimpleView {
-  
+/* Program created by Thomas Baines
+for use in BCPR280 - Software Engineering 2; Assignment 2
+All rights reserved */
+
+/* Updated to conform to standardJS 02/08/2018 5:31pm */
+
+class SimpleView { // eslint-disable-line no-unused-vars
   static get (thePrompt) {
     // method to get input from the user and return it as a variable
     // no error checking
     return window.prompt(thePrompt)
   }
-  
+
   static out (outString) {
     // method to output text to the user
     window.alert(outString)
   }
-  
-  
-  static highLowRuntime(highLow) {
+
+  static highLowRuntime (highLow) {
     // method to run a loop to take input from the user and run HighLowGuess
-    
+
     // first cycle outside of loop + value declarations
     let theGuess = this.get(highLow.firstPrompt) // get user input
     if (theGuess === null) { // allow the user to break out of the input loop
       return false
     }
     let theResponse = highLow.guess(theGuess) // pass user input to HighLowGuess and get the response
-    let exit = theResponse[0] // activate exit condition for the loop 
-    
-    while(!exit) { // continue untill the guess is correct
+    let exit = theResponse[0] // activate exit condition for the loop
+
+    while (!exit) { // continue untill the guess is correct
       theGuess = this.get(theResponse[1]) // ask for user input, passing the prompt provided from the first guess
       if (theGuess === null) { // allow the user to break out of the input loop
-      return false
-    }
+        return false
+      }
       theResponse = highLow.guess(theGuess) // pass the new user input back to HighLowGuess
       exit = theResponse[0] // update exit condition
     }
     this.out(theResponse[1]) // final state - this does not require further user input
-    
   }
-  
-  
 }
