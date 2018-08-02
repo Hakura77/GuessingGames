ce6e2992390c5f5f5956048c8675b885d9909304
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-08-02 15:53:12 +1200

    Created HighLowRuntime

--- src/SimpleView.js
+++ src/SimpleView.js
@@ -1,18 +1,33 @@
 class SimpleView {
   
-  static get (prompt) {
+  static get (thePrompt) {
     // method to get input from the user and return it as a variable
     // no error checking
-    return window.prompt(prompt)
+    return window.prompt(thePrompt)
   }
   
   static out (outString) {
     // method to output text to the user
     window.alert(outString)
-    
-    
   }
   
+  
+  static highLowRuntime(highLow) {
+    // method to run a loop to take input from the user and run HighLowGuess
+    
+    // first cycle outside of loop + value declarations
+    let theGuess = this.get(highLow.firstPrompt) // get user input
+    let theResponse = highLow.guess(theGuess) // pass user input to HighLowGuess and get the response
+    let exit = theResponse[0] // activate exit condition for the loop 
+    
+    while(!exit) { // continue untill the guess is correct
+      theGuess = this.get(theResponse[1]) // ask for user input, passing the prompt provided from the first guess
+      theResponse = highLow.guess(theGuess) // pass the new user input back to HighLowGuess
+      exit = theResponse[0] // update exit condition
+    }
+    this.out(theResponse[1]) // final state - this does not require further user input
+    
+  }
   
   
 }
