c0b2fa902e2f78b29a35b35aa0d5dba9478b2291
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-08-02 16:02:53 +1200

    added logic to allow the user to break out of the input loop

--- src/SimpleView.js
+++ src/SimpleView.js
@@ -17,11 +17,17 @@ class SimpleView {
     
     // first cycle outside of loop + value declarations
     let theGuess = this.get(highLow.firstPrompt) // get user input
+    if (theGuess === null) { // allow the user to break out of the input loop
+      return false
+    }
     let theResponse = highLow.guess(theGuess) // pass user input to HighLowGuess and get the response
     let exit = theResponse[0] // activate exit condition for the loop 
     
     while(!exit) { // continue untill the guess is correct
       theGuess = this.get(theResponse[1]) // ask for user input, passing the prompt provided from the first guess
+      if (theGuess === null) { // allow the user to break out of the input loop
+      return false
+    }
       theResponse = highLow.guess(theGuess) // pass the new user input back to HighLowGuess
       exit = theResponse[0] // update exit condition
     }
