be4f995146dab77a9abea178584a73154870f3f6
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-08-02 17:29:44 +1200

    Error fix and StandardJS
    Fixed logic error caused by not using isNaN() call to check for something being NaN. Also update to conform to standardJS

--- src/HighLowGuess.js
+++ src/HighLowGuess.js
@@ -1,23 +1,27 @@
-class HighLowGuess {
+/* Program created by Thomas Baines
+for use in BCPR280 - Software Engineering 2; Assignment 2
+All rights reserved */
+
+/* Updated to conform to standardJS 2/08/2018 5:28pm */
+
+class HighLowGuess { // eslint-disable-line no-unused-vars
   constructor () {
     this.answer = Math.floor(Math.random() * 100)
     this.guessCount = 0
     this.firstPrompt = 'Guess a number between 0 and 99'
   }
-  
-  
-  
-  guess(newGuess) {
+
+  guess (newGuess) {
     // increment guess counter
     this.guessCount += 1
-    
+
     // input validation
-    newGuess = parseInt(newGuess) // converts newGuess into a number
-    if (newGuess != 'NaN') { // checks if the number is NaN - can't check typeof here because JS is dumb
-      if (newGuess % 1 != 0) { 
+    newGuess = Number(newGuess) // converts newGuess into a number
+    if (!isNaN(newGuess)) { // checks if the number is NaN - can't check typeof here because JS is dumb
+      if (newGuess % 1 !== 0) {
         // number had decimal points
-        return [false, "Please enter a whole number"]
-      } else if (newGuess < 0 || newGuess > 99 ) {
+        return [false, 'Please enter a whole number']
+      } else if (newGuess < 0 || newGuess > 99) {
         // number was negative
         return [false, 'Please enter a number between 0 and 99']
       } else {
@@ -25,22 +29,15 @@ class HighLowGuess {
       }
     } else {
     // guess was a non-number datatype - possible edge case where a date is passed but that would be wrong anyway
-    return [false, "Please enter a number"]
+      return [false, 'Please enter a number']
     }
-    
+
     if (this.answer > newGuess) { // number was too high
-      return [false, "Try higher"]
-      
+      return [false, 'Try higher']
     } else if (this.answer < newGuess) { // number was too low
-      return [false, "Try lower"]
-      
+      return [false, 'Try lower']
     } else { // guess was correct
       return [true, `You got it in ${this.guessCount} tries`]
     }
   }
-  
-  
-  
-  
-  
 }
