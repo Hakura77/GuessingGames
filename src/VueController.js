d438ef9ff41fa4029fc9984a8c95603a7d6000c5
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-08-10 23:17:32 +1200

    Added VueController
    Created VueController class and added some base code

---
+++ src/VueController.js
@@ -0,0 +1,18 @@
+class VueController() {
+  constructor () {
+    this.allMyElements = new Map()
+    this.currentGame = undefined
+    this.initalize()
+  }
+  
+  initalize() {
+    // method to initalize the vue element bindings and also set currentGame correctly
+    return false 
+    
+  }
+  
+  
+  
+  
+  
+}
