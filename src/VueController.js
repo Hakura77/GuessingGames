953e6d8d811b6f68dd9e4e3480c46dd36f63e010
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-08-14 09:54:45 +1200

    updates to stylesheet begin building html

--- src/VueController.js
+++ src/VueController.js
@@ -1,3 +1,27 @@
+/* 
+will work on this later once more has been learned about vue
+
+Vue.component(
+  'gameHeader', {
+    props: titlePrompt
+    template: '<h1>{{ titlePrompt }}</h1>'
+  }
+)
+Vue.component(
+  'gameLog', {
+    props: [messageLog],
+    template: "<ul id='chatLog'> <li v-for='message in messageLog'> message.value </li> </ul>"
+  }
+)
+
+Vue.component (
+  'gameInput' {
+    
+    
+  }
+
+) */
+
 class VueController() {
   constructor () {
     this.allMyElements = new Map()
@@ -5,9 +29,33 @@ class VueController() {
     this.initalize()
   }
   
+  vContSwitchTo(target) {
+    this.allMyElements.get(target).enabled = true
+  }
+  
+  
+  
   initalize() {
     // method to initalize the vue element bindings and also set currentGame correctly
-    return false 
+    
+    this.allMyElements.set( // create sideBar bound vue element
+      highLowGuess,
+      new Vue ({
+        el: sidebar,
+        data: {
+          
+        },
+        methods {
+          switchTo: function(target) {
+            this.vContSwitchTo(target)
+          }
+        }
+        
+        
+      })
+    )
+    
+    
     
   }
   
